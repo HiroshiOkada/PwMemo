6444d86b645d01937a6382a6397494c9b4f49c83
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-04-20 21:37:53 +0900

    LockImageView をクラスとして作成することにし、レイアウトを調整した

---
+++ src/com/toycode/idpw/LockImageView.java
@@ -0,0 +1,23 @@
+package com.toycode.idpw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class LockImageView extends ImageView {
+
+	public LockImageView(Context context) {
+		super(context);
+	}
+
+	public LockImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public LockImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+
+}
