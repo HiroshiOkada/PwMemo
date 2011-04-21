e570ff764646c2e15a932822299ed543b88f554e
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-04-21 22:43:32 +0900

    マスターパスワードを入力してもらう機能をクラスとして作成

---
+++ src/com/toycode/idpw/MasterPasswordInput.java
@@ -0,0 +1,94 @@
+package com.toycode.idpw;
+
+import java.lang.reflect.Method;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * ダイアログを表示し、マスターパスワードを入力してもらう
+ * 機能を実現するクラス
+ * 
+ * Activity の中で次の用に呼び出す
+ * MasterPasswordInput mpi = new MasterPasswordInput () {
+ *   public void onTureMasterPassword() {
+ *     正しいパスワードが入力された時の処理
+ *   }
+ * };
+ * mpi.Ask()
+ * 
+ * @author hiroshi
+ *
+ */
+public abstract class MasterPasswordInput {
+	
+	Activity mActivity;
+
+	public MasterPasswordInput(Activity activity) {
+		mActivity = activity;
+	}
+
+	/**
+	 * ダイアログを表示し、マスターパスワードを入力してもらう
+	 */
+	public void Ask() {
+		final EditText editText = new EditText(mActivity);
+		
+		editText.setTransformationMethod(PasswordTransformationMethod
+				.getInstance());
+		final AlertDialog alertDialog = new AlertDialog.Builder(mActivity)
+				.setTitle(mActivity.getString(R.string.input_masterpassword))
+				.setView(editText).setPositiveButton(android.R.string.ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								onPassword( editText.getText().toString());
+							}
+						}).setNegativeButton(android.R.string.cancel, null)
+				.create();
+
+		editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					alertDialog
+							.getWindow()
+							.setSoftInputMode(
+									WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+				}
+			}
+		});
+		alertDialog.show();
+	}
+
+	/**
+	 * password が入力されたときの処理
+	 */
+	private void onPassword( String password) {
+		
+		PasswordManager mPasswordManager = PasswordManager.getInstance(mActivity);
+		if( mPasswordManager.isMainPasswordExist()){
+			if( mPasswordManager.decryptMainPassword(password) == null){
+				Toast.makeText(mActivity, "Password not match", Toast.LENGTH_LONG).show();
+				return;
+			} else {
+				onTureMasterPassword();
+			}
+
+		}else{
+			mPasswordManager.createMainPassword(password);
+			Toast.makeText(mActivity, "Set master password", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	/**
+	 * 正しいパスワードが入力された時に呼び出される
+	 * @return
+	 */
+	abstract void onTureMasterPassword();
+}
