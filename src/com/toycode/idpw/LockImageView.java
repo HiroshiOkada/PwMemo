67a8287381b7e59583340f17011321ff468d913d
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-05-09 20:45:24 +0900

    import の整理

--- src/com/toycode/idpw/LockImageView.java
+++ src/com/toycode/idpw/LockImageView.java
@@ -2,9 +2,7 @@ package com.toycode.idpw;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class LockImageView extends Button {
 
