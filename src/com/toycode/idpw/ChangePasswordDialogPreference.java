93344d1c44dbe8aa06e9baa039ef0bcc5f11b62c
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-05-29 23:32:31 +0900

    Preference 作成開始

---
+++ src/com/toycode/idpw/ChangePasswordDialogPreference.java
@@ -0,0 +1,18 @@
+package com.toycode.idpw;
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+
+public class ChangePasswordDialogPreference extends DialogPreference {
+
+	public ChangePasswordDialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public ChangePasswordDialogPreference(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+}
