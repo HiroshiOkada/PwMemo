a6863f7189952fd1ad1874f33fb57b80990d02d5
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-06-26 15:59:16 +0900

    フィールドの設定が readonly になっていたのを解除

--- src/com/toycode/idpw/IdPwEditActivity.java
+++ src/com/toycode/idpw/IdPwEditActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
