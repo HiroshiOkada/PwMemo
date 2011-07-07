f0e916ba80e6fdc73e38c3e139e2ac5d7eb8367e
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-07-08 06:59:53 +0900

    viewstub を使うのではなく、レイアウト切り替えにした

--- src/com/toycode/idpw/ExportActivity.java
+++ src/com/toycode/idpw/ExportActivity.java
@@ -1,3 +1,4 @@
+
 package com.toycode.idpw;
 
 import android.app.Activity;
@@ -5,29 +6,40 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewStub;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import java.util.List;
 
-public class ExportActivity extends Activity {
-    
+public class ExportActivity extends Activity implements OnClickListener {
+
     public static final String OI_ACTION_PICK_FILE = "org.openintents.action.PICK_FILE";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.export);
+
         if (checkOIActionPickFile()) {
-            Button b = (Button)((ViewStub) findViewById(R.id.selectfile_viewstub)).inflate();
+            setContentView(R.layout.export);
+        } else {
+            setContentView(R.layout.export_input_filename);
         }
+        findViewById(R.id.write_file_button).setOnClickListener(this);
     }
-    
-    
+
     private boolean checkOIActionPickFile() {
         Intent intent = new Intent(OI_ACTION_PICK_FILE);
         PackageManager pm = getPackageManager();
-        List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        List<ResolveInfo> list = pm
+                .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
         return list.size() > 0;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
     }
 }
