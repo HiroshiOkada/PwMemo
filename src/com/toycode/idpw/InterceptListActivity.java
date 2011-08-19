4301e540e849ef7466d7d220d16192afde8bd560
Hiroshi Okada <okadahiroshi@miobox.jp>
2011-08-19 18:33:19 +0900

    work as Mushroom

--- src/com/toycode/idpw/InterceptListActivity.java
+++ src/com/toycode/idpw/InterceptListActivity.java
@@ -1,4 +1,168 @@
+
 package com.toycode.idpw;
 
-public class InterceptListActivity extends MainListActivity {
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView;
+import android.widget.SimpleCursorAdapter;
+
+public class InterceptListActivity extends AbstractMainListActivity {
+    
+    private static final String REPLACE_KEY = "replace_key";
+
+    String mSearchString = "";
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSearchString = getIntent().getStringExtra(REPLACE_KEY);
+        if (mSearchString != null) {
+            setTitle( getTitle() + " " + mSearchString);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        findViewById(R.id.add_button).setVisibility(View.GONE);
+        if (isLocked()) {
+            (new MasterPasswordInput(this) {
+                public void onTureMasterPassword() {
+                    updateLockImageButton();
+                }
+            }).Ask();
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+            long id) {
+        TimeOutChecker.getInstance().onUser();
+        if (isLocked()) {
+            Toy.toastMessage(this, R.string.locked_message);
+        } else {
+            this.itemClickNormalTask(position, id);
+        }
+    }
+
+    /**
+     * Called when a list item long-pressed and context menu is about to be shown.
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        createContextMenuTask(menu, v, menuInfo, R.menu.list_item_mushroom_menu);
+     }    
+    
+    /**
+     * Called when a context menu item selected
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        TimeOutChecker.getInstance().onUser();
+
+        // If locked do nothing. (maybe never occur.)
+        if (isLocked()) {
+            Toy.debugLog(this, "onContextItemSelected and locked");
+            return false;
+        }
+
+        Cursor cursor = getCursorFromMenuInfo(item.getMenuInfo());
+        if (cursor == null) {
+            Toy.debugLog(this, "bad cursor");
+            return false;
+        }
+        Long id = cursor.getLong(0);
+        DbRw.Data data = getData(id);
+        if (data == null) {
+            return false;
+        }
+        switch (item.getItemId()) {
+            case R.id.insert_userid_menu_item:
+                finishWithResult(data.getUserId());
+                break;
+            case R.id.insert_password_menu_item:
+                finishWithResult(data.getPassword());
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+    
+    /**
+     * No AddButton
+     */
+   protected void onAddButton() {
+    }
+   
+   /**
+    * ExitButton
+    * return "" string as result
+    */
+   protected void onExitButton() {
+       PasswordManager.getInstance(this).unDecrypt();
+       finishWithResult("");
+   }
+   
+    @Override
+    protected void itemClickNormalTask(int position, long id) {
+        String title = "";
+        Cursor cursor = (Cursor) (getListAdapter().getItem(position));
+        if (cursor != null) {
+            title = cursor.getString(1);
+        } else {
+            Toy.debugLog(this, "bad cursor");
+        }
+
+        DbRw.Data data = getData(id);
+        if (data != null) {
+            Toy.toastMessage(this, R.string.copy_x_password, title);
+            finishWithResult(data.getPassword());
+        }
+    }
+ 
+    @Override    
+    protected void updateAdapter() {
+        if (Toy.isEmptyCharSequence(mSearchString)) {
+            super.updateAdapter();
+            return;
+        }
+        
+        final String[] COLUMNS = {
+                Const.COLUMN.ID, Const.COLUMN.TITLE
+        };
+        final String WHERE = Const.COLUMN.TITLE + " LIKE ?"; 
+        final String[] FROM = {
+            Const.COLUMN.TITLE
+        };
+        final int[] TO = {
+            R.id.RowTitleTextView
+        };
+        String [] whereArgs = { "%" + mSearchString + "%"};
+        Cursor cursor = mDb.query(
+                Const.TABLE.IDPW,    // table 
+                COLUMNS,             // columns
+                WHERE,               // selection
+                whereArgs,           // selectionArgs
+                null,               // groupBy
+                null,               // having
+                Const.COLUMN.TITLE + " COLLATE NOCASE"    // orderBy
+                );
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_row, cursor,
+                FROM, TO);
+        setListAdapter(adapter);
+    }
+
+    private void finishWithResult(String result) {
+        Intent intent = new Intent();
+        intent.putExtra(REPLACE_KEY, result);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
--- src/com/toycode/idpw/MainListActivity.java
+++ src/com/toycode/idpw/MainListActivity.java
@@ -1,130 +1,15 @@
 
 package com.toycode.idpw;
 
-import android.app.ListActivity;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.AdapterView.OnItemClickListener;
 
-import java.util.Observable;
-import java.util.Observer;
-
-public class MainListActivity extends ListActivity implements OnClickListener,
-        OnItemClickListener, Observer {
-
-    LockImageButton mLockImageButton;
-    SQLiteDatabase mDb;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        IdPwPreferenceActivity.initPreferences(this);
-        TimeOutChecker.getInstance().setUseTimeOut(IdPwPreferenceActivity.getAutoLock(this));
-        TimeOutChecker.getInstance().setTimeOutSec(IdPwPreferenceActivity.getAutoLockTime(this));
-        
-        setContentView(R.layout.list);
-
-        mLockImageButton = (LockImageButton) findViewById(R.id.lock_image_button);
-        updateLockImageButton();
-        mLockImageButton.setOnClickListener(this);
-
-        findViewById(R.id.add_button).setOnClickListener(this);
-        findViewById(R.id.exit_button).setOnClickListener(this);
-
-        ListView listView = getListView();
-        listView.setEmptyView(findViewById(R.id.EmptyTextView));
-        listView.setOnItemClickListener(this);
-        registerForContextMenu(listView);
-
-        mDb = (new IdPwDbOpenHelper(this)).getWritableDatabase();
-        updateAdapter();
-
-    }
-       
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (TimeOutChecker.getInstance().isTimeOut()) {
-            PasswordManager.getInstance(this).unDecrypt();
-        }
-        if (!PasswordManager.getInstance(this).isMainPasswordExist()) {
-            Intent i = new Intent(this, DeclarMasterPasswordActivity.class);
-            startActivityForResult(i, Const.REQUEST_TYPE.NEW);
-        } else {
-            mLockImageButton.setLock(isLocked());
-            updateAdapter();
-        }
-        TimeOutChecker.getInstance().addObserver(this);
-    }
-
-    @Override
-    protected void onPause() {
-        TimeOutChecker.getInstance().deleteObserver(this);
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        // TODO Auto-generated method stub
-        super.onDestroy();
-    }
-
-    private void updateAdapter() {
-        final String[] COLUMNS = {
-                Const.COLUMN.ID, Const.COLUMN.TITLE
-        };
-        final String[] FROM = {
-            Const.COLUMN.TITLE
-        };
-        final int[] TO = {
-            R.id.RowTitleTextView
-        };
-        Cursor cursor = mDb.query(
-                Const.TABLE.IDPW,    // table 
-                COLUMNS,             // columns
-                null,               // selection
-                null,               // selectionArgs
-                null,               // groupBy
-                null,               // having
-                Const.COLUMN.TITLE + " COLLATE NOCASE"    // orderBy
-                );
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_row, cursor,
-                FROM, TO);
-        setListAdapter(adapter);
-    }
-
-    /**
-     * 各ボタンが押されたときの処理 ボタンに合わせたメソッドを呼び出す。
-     */
-    @Override
-    public void onClick(View v) {
-        TimeOutChecker.getInstance().onUser();
-        switch (v.getId()) {
-            case R.id.lock_image_button:
-                onLockImageButton();
-                break;
-            case R.id.add_button:
-                onAddButton();
-                break;
-            case R.id.exit_button:
-                onExitButton();
-                break;
-        }
-    }
+public class MainListActivity extends AbstractMainListActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -140,58 +25,6 @@ public class MainListActivity extends ListActivity implements OnClickListener,
                     updateAdapter();
                 }
                 break;
-        }
-    }
-
-    /**
-     * LockImageButton が押された時の処理
-     */
-    private void onLockImageButton() {
-        if (isLocked()) {
-            (new MasterPasswordInput(this) {
-                public void onTureMasterPassword() {
-                    updateLockImageButton();
-                }
-            }).Ask();
-        } else {
-            PasswordManager.getInstance(this).unDecrypt();
-            updateLockImageButton();
-        }
-    }
-
-    /**
-     * AddButton が押された時の処理
-     */
-    private void onAddButton() {
-        if (isLocked()) {
-            Toy.toastMessage(this, R.string.locked_message);
-        } else {
-            ContentValues values = new ContentValues();
-            values.put(Const.COLUMN.TITLE, "");
-            long id = mDb.insert(Const.TABLE.IDPW, null, values);
-            startEditActivity(id, Const.REQUEST_TYPE.NEW);
-        }
-    }
-
-    /**
-     * ExitButto が押された時の処理
-     */
-    private void onExitButton() {
-        PasswordManager.getInstance(this).unDecrypt();
-        finish();
-    }
-
-    /**
-     * アイテムがクリックされた時の処理 Unlock 状態なら編集画面に遷移、そうでなければ、lock されている旨を表示
-     */
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position,
-            long id) {
-        TimeOutChecker.getInstance().onUser();
-        if (isLocked()) {
-            Toy.toastMessage(this, R.string.locked_message);
-        } else {
-            startEditActivity(getListAdapter().getItemId(position), Const.REQUEST_TYPE.EDIT);
         }
     }
 
@@ -248,68 +81,38 @@ public class MainListActivity extends ListActivity implements OnClickListener,
      */
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-        TimeOutChecker.getInstance().onUser();
-        //  If locked Show the toast message and do nothing.
-        if (isLocked()) {
-            Toy.toastMessage(this, R.string.locked_message);
-            return;
-        }
-
-        // Create context nemu
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.list_item_menu, menu);
-       
-        // set menu title
-        if (!(menuInfo instanceof AdapterContextMenuInfo)) {
-            Toy.debugLog(this, "bad menuInfo");
-            return;
-        }
-        int position = ((AdapterContextMenuInfo)menuInfo).position;
-        Cursor cursor = (Cursor)(getListAdapter().getItem(position));
-        if (cursor != null) {
-            menu.setHeaderTitle(cursor.getString(1));
-        } else {
-            Toy.debugLog(this, "bad cursor");
-        }
+        createContextMenuTask(menu, v, menuInfo, R.menu.list_item_menu);
      }
-
+    
     /**
      * Called when a context menu item selected 
      */
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         TimeOutChecker.getInstance().onUser();
-
+        
         //  If locked do nothing. (maybe never occur.)
         if (isLocked()) {
             Toy.debugLog(this, "onContextItemSelected and locked");
             return false;
         }
        
-        // get Id and title
-        ContextMenuInfo menuInfo = item.getMenuInfo();
-        if (!(menuInfo instanceof AdapterContextMenuInfo)) {
-            return false;
-        }
-        int position = ((AdapterContextMenuInfo)menuInfo).position;
-        Cursor cursor = (Cursor)(getListAdapter().getItem(position));
+        Cursor cursor = getCursorFromMenuInfo(item.getMenuInfo());
         if (cursor == null) {
             Toy.debugLog(this, "bad cursor");
             return false;
         }
         Long id = cursor.getLong(0);
         String title = cursor.getString(1);
-        DbRw.Data data;
+        DbRw.Data data = getData(id);
         switch (item.getItemId()) {
             case R.id.copy_userid_menu_item:
-                data = getData(id);
                 if (data != null) {
                     Toy.copyTextToClipboard(this, data.getUserId());
                     Toy.toastMessage(this, R.string.copy_x, data.getUserId());
                 }
                 break;
             case R.id.copy_password_menu_item:
-                data = getData(id);
                 if (data != null) {
                     Toy.copyTextToClipboard(this, data.getPassword());
                     Toy.toastMessage(this, R.string.copy_x_password, title);
@@ -324,50 +127,8 @@ public class MainListActivity extends ListActivity implements OnClickListener,
         return true;
     }
 
-    /**
-     * read data spcified id
-     */
-    private DbRw.Data getData(long id) {
-        byte[] mainPasswod = PasswordManager.getInstance(this).getDecryptedMainPassword();
-        if (mainPasswod == null) {
-            setResult(RESULT_CANCELED, new Intent());
-            finish();
-            return null;
-        }
-        DbRw dbrw = new DbRw(mDb, mainPasswod);
-        return dbrw.getRecord(id);
-    }
-    
-    /**
-     * start EditActivity via intent.
-     */
-    private void startEditActivity(long id, int requestCode) {
-        Intent intent = new Intent(this, EditActivity.class);
-        intent.putExtra(Const.COLUMN.ID, id);
-        intent.putExtra(Const.REQUEST_TYPE.NAME, requestCode);
-        startActivityForResult(intent, requestCode);
-    }
-
-    /**
-     * return ture when lockd
-     */
-    private boolean isLocked() {
-        return ! (PasswordManager.getInstance(this).isMainPasswordDecrypted());
-    }
-   
-    /**
-     * 現在のマスターパスワードの状況に応じて LockImageButton　を変化させる
-     */
-    private void updateLockImageButton() {
-        mLockImageButton
-                .setLock(PasswordManager.getInstance(this).isMainPasswordDecrypted() == false);
-    }
-
     @Override
-    public void update(Observable observable, Object data) {
-        if( TimeOutChecker.getInstance().isTimeOut()) {
-            PasswordManager.getInstance(this).unDecrypt();
-            updateLockImageButton();    
-        }
+    protected void itemClickNormalTask(int position, long id) {
+        startEditActivity(getListAdapter().getItemId(position), Const.REQUEST_TYPE.EDIT);        
     }
 }
